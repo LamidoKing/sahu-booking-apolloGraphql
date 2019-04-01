1598ba1862f9484ee14ff3b20f50d6aa525e8772
Lamido <kinglamido@gmail.com>
2019-04-01 21:15:24 +0100

    sahu apollo server booking app

--- src/index.js
+++ src/index.js
@@ -14,57 +14,51 @@ import {
 } from './config'
 import schemaDirectives from './CustomDirective'
 
-(async () => {
-  try {
-    await mongoose.connect(
-      `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-      {
-        useNewUrlParser: true
-      }
-    )
-
-    const PORT = process.env.PORT || 4000
-    const app = express()
-
-    app.disable('x-powered-by')
-    const RedisStore = connectRedis(session)
-    const store = new RedisStore({
-      host: REDI_HOST,
-      port: REDIS_PORT,
-      pass: REDIS_PASSWORD
-    })
-
-    app.use(cors())
-
-    app.use(session({
-      store,
-      name: SESS_NAME,
-      secret: SESS_SECRET,
-      resave: true,
-      rolling: true,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: parseInt(SESS_LIFETIME),
-        sameSite: true,
-        secure: PROD
-      }
-    }))
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-      schemaDirectives,
-      playground: PROD ? false : {
-        'request.credentials': 'include'
-      },
-      context: ({ req, res }) => ({ req, res })
-    })
-
-    server.applyMiddleware({ app, cors: false })
-
-    app.listen({ port: PORT }, () =>
-      console.log(`http://localhost:${PORT}${server.graphqlPath}`)
-    )
-  } catch (e) {
-    console.error(e)
+mongoose.connect(
+  `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  {
+    useNewUrlParser: true
   }
-})()
+)
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.disable('x-powered-by')
+const RedisStore = connectRedis(session)
+const store = new RedisStore({
+  host: REDI_HOST,
+  port: REDIS_PORT,
+  pass: REDIS_PASSWORD
+})
+
+app.use(cors())
+
+app.use(session({
+  store,
+  name: SESS_NAME,
+  secret: SESS_SECRET,
+  resave: true,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: parseInt(SESS_LIFETIME),
+    sameSite: true,
+    secure: PROD
+  }
+}))
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  schemaDirectives,
+  playground: PROD ? false : {
+    'request.credentials': 'include'
+  },
+  context: ({ req, res }) => ({ req, res })
+})
+
+server.applyMiddleware({ app, cors: false })
+
+app.listen({ port: PORT }, () =>
+  console.log(`http://localhost:${PORT}${server.graphqlPath}`)
+)
